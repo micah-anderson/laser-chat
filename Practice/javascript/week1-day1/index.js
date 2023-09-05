0e5f33ab70c7735b18af38faac808bbc0b25274d
Micah Anderson <micah.anderson@dci-student.org>
2023-09-05 12:52:23 +0200

    Finish Excercise 0.0

---
+++ Practice/javascript/week1-day1/index.js
@@ -0,0 +1,36 @@
+function doThisExcerciseAndMore() {
+    let thereBe = "LIGHT!";
+    console.log(thereBe);
+    const realAge = "eternal";
+    let age = 2;
+    console.log(realAge);
+    console.log(age)
+    let greeting = "hello";
+    let hamburgGreeting = "moin!";
+    let number = 5;
+    console.log(greeting);
+    console.log(hamburgGreeting);
+    console.log(number);
+    console.log(typeof.5);
+
+    let isSuperCool = true;
+    console.log(isSuperCool);
+    let firstName = "Mr."; //will probably change my name sometime in the future so no const used here 👍
+    let lastName = "Anderson😎";
+    let currentCity = "Bispingen :cityscape:"
+    let favBand = "🎹very hard to say🎵" ;
+    let favMovie = "😵‍💫i dunno";
+    let favBook = "💯The Bible💯";
+
+
+    console.log(lastName);
+    console.log(currentCity);
+    console.log(favBand);
+    console.log(favMovie);
+    console.log(favBook);
+
+    console.log(lastName, currentCity, favBand, favMovie, favBook
+    );
+}
+
+doThisExcerciseAndMore();
